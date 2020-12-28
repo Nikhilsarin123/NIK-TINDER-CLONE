f917f880fde026e04ff3a7019246e8ef88136970
nikhilsarin <nikhilsarin97@gmail.com>
2020-12-29 02:58:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+import SwipeButton from "./SwipeButton";
+
+
+function App() {
+  return (
+    //bem class
+    <div className="app">
+   
+ <Header />
+  <TinderCards />
+  <SwipeButton />
+    </div>   
+  );
+}
+
+export default App;
